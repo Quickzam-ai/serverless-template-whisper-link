3d7c5f8783dc3c90b003e526e8a5d0879f74d76e
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-10-15 14:18:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,35 @@ import whisper
 import os
 import base64
 from io import BytesIO
+from pytube import YouTube 
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
     
-    model = whisper.load_model("base")
+    model = whisper.load_model("large")
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: link) -> dict:
     global model
 
     # Parse out your arguments
-    mp3BytesString = model_inputs.get('mp3BytesString', None)
-    if mp3BytesString == None:
-        return {'message': "No input provided"}
+#     mp3BytesString = model_inputs.get('mp3BytesString', None)
+#     if mp3BytesString == None:
+#         return {'message': "No input provided"}
     
-    mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
-    with open('input.mp3','wb') as file:
-        file.write(mp3Bytes.getbuffer())
+#     mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
+#     with open('input.mp3','wb') as file:
+#         file.write(mp3Bytes.getbuffer())
     
     # Run the model
-    result = model.transcribe("input.mp3")
+    yt = YouTube(model_inputs)
+    audio = whisper.load_audio(yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download())
+    translate_options = dict(task="translate")
+    result = model.transcribe(audio, **translate_options)
     output = {"text":result["text"]}
-    os.remove("input.mp3")
+    #os.remove("input.mp3")
     # Return the results as a dictionary
     return output
