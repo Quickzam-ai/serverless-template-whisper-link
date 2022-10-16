23f9f35d57c85f4d40b0e2d270377c709b4232af
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-10-16 09:41:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def init():
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs: link) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
 
-    # Parse out your arguments
+    #Parse out your arguments
 #     mp3BytesString = model_inputs.get('mp3BytesString', None)
 #     if mp3BytesString == None:
 #         return {'message': "No input provided"}
@@ -25,9 +25,10 @@ def inference(model_inputs: link) -> dict:
 #     mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
 #     with open('input.mp3','wb') as file:
 #         file.write(mp3Bytes.getbuffer())
+
     
     # Run the model
-    yt = YouTube(model_inputs)
+    yt = YouTube(dict.get("myByteString"))
     audio = whisper.load_audio(yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download())
     translate_options = dict(task="translate")
     result = model.transcribe(audio, **translate_options)
